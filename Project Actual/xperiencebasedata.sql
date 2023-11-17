ab680aea229f4d854e23ab9aaa37661a20ae3673
dq2400 <92576351+dq2400@users.noreply.github.com>
2023-11-17 14:22:25 +0800

    Update xperiencebasedata.sql
    Bug fixed

--- Project Actual/xperiencebasedata.sql
+++ Project Actual/xperiencebasedata.sql
@@ -1,7 +1,7 @@
 use xperience_db;
 
 insert into xperiencerun values
-  (1, "2023-10-09", "2023-10-20", "08:00:00", "10:00:00", "12:00:00", "14:00:00", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 10),
+  (1, "2023-10-09", "2023-10-20", "08:00:00", "10:00:00", "12:00:00", "14:00:00", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 10);
 insert into logincredentials values
   ("admin", MD5("123"), "admin"),
   ("tohys005", MD5("1qaz"), "student"),
